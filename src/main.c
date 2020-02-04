d3bec66c0d270454cd77a6ea826a34606b5852f2
Anton <goloviznin1995@yandex.ru>
2020-02-04 07:56:32 +0300

    Removed unjustified global variable g_settings

--- src/main.c
+++ src/main.c
@@ -5,12 +5,12 @@ int			ft_putint(int c)
 	return (write(STDOUT_FILENO, &c, 1));
 }
 
-void		set_term_settings(void)
+void		set_term_settings(struct termios *old_settings)
 {
 	struct termios	new_settings;
 
-	tcgetattr(STDIN_FILENO, &g_settings);
-	new_settings = g_settings;
+	tcgetattr(STDIN_FILENO, old_settings);
+	new_settings = *old_settings;
 	new_settings.c_lflag &= ~ICANON;
 	new_settings.c_lflag &= ~ECHO;
 	new_settings.c_cc[VTIME] = 0;
@@ -18,9 +18,9 @@ void		set_term_settings(void)
 	tcsetattr(STDIN_FILENO, TCSANOW, &new_settings);
 }
 
-void	    return_term_settings(void)
+void	    return_term_settings(struct termios *old_settings)
 {
-	tcsetattr(STDIN_FILENO, TCSANOW, &g_settings);
+	tcsetattr(STDIN_FILENO, TCSANOW, old_settings);
 }
 
 void        start_program(char **env)
@@ -40,6 +40,7 @@ int         main(int ac, char **av, char **environ)
 {
     char	*termtype;
 	char	room_termtype[2048];
+	struct termios term_settings;
 
 	termtype = getenv("TERM");
 	if (termtype == NULL || tgetent(room_termtype, termtype) != 1)
@@ -49,8 +50,8 @@ int         main(int ac, char **av, char **environ)
     }
     (void)ac;
     (void)av;
-    set_term_settings();
+    set_term_settings(&term_settings);
     start_program(environ);
-    return_term_settings();
+    return_term_settings(&term_settings);
     return (0);
 }
