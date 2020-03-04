1fe1a5b5b72a1ac0be62da105ce65bc741db9ca4
tsorion <tsorion_9@mail.ru>
2020-03-04 18:35:53 +0300

    wpwpqqp

--- src/readline/user_in_editing.c
+++ src/readline/user_in_editing.c
@@ -46,7 +46,6 @@ void        delete_symbol_sup(char *user_in, int *cur_pos, int i, int *n)
     *n = cur_pos[0];
     if (user_in[i] == '\n')
     {
-        tc_clear_till_end();
         tc_cursor_up(cur_pos);
         if (cur_pos[1] == 1)
         {
@@ -126,99 +125,22 @@ void        delete_last_word(char *user_in, int *cur_pos)
 ** Добавляет символ и печатает строку
 */
 
-static void  insert_symbol_sup(char *user_in, int *cur_pos, char c, int a)
+void        insert_symbol(char *user_in, int *cur_pos, char c)
 {
-    int     len;
     char    *str;
-    int     i;
     int     tmp[2];
-    int     n;
+    int     i;
 
-    i = 0;
-    n = 1;
-    while (n != *(cur_pos + 1))
-    {
-        if (user_in[i] == '\n')
-            n++;
-        i++;
-    }
-    n = 1;
-    while (n < *cur_pos - a)
-    {
-        n++;
-        i++;
-    }
+    i = search_index(user_in, cur_pos);
     str = ft_strdup(user_in + i);
     ft_strcpy(user_in + i + 1, str);
     free(str);
     user_in[i] = c;
-    tmp[1] = *cur_pos;
-    tmp[2] = *(cur_pos + 1);
+    tmp[0] = cur_pos[0];
+    tmp[1] = cur_pos[1];
     clear_all_line(user_in, cur_pos);
-    *(cur_pos) = tmp[1];
-    *(cur_pos + 1) = tmp[2];
+    cur_pos[0] = tmp[0] + 1;
+    cur_pos[1] = tmp[1];
     ft_putstr(user_in);
-    if (str_n(user_in) + 1 > *(cur_pos + 1))
-    {
-        i = str_n(user_in) + 1;
-        while (i > *(cur_pos + 1))
-        {
-            tc_cursor_up(NULL);
-            tc_cursor_up(NULL);
-            write(STDOUT_FILENO, "\n", 1);
-            i--;
-        }
-        i = 0;
-        while (i < *(cur_pos))
-        {
-            tc_cursor_right(NULL);
-            i++;
-        }
-    }
-    else
-    {
-        i = ft_strlen(user_in);
-        len = 0;
-        while (user_in[i] != '\n')
-        {
-            i--;
-            len++;
-            if (i == 0)
-                break ;
-        }
-        if (*(cur_pos + 1) != 1)
-            len--;
-        while (len + a > *cur_pos)
-        {
-		    tc_cursor_left(NULL);
-            len--;
-        }
-    }
-    (*cur_pos)++;
-}
-
-void        insert_symbol(char *user_in, int *cur_pos, char c)
-{
-    int n;
-    int i;
-
-    if (ft_strchr(user_in, '\n') == NULL)
-        insert_symbol_sup(user_in, cur_pos, c, 2);
-    else
-    {
-        i = 0;
-        n = 1;
-        while (n != *(cur_pos + 1))
-        {
-            if (user_in[i] == '\n')
-                n++;
-            i++;
-        }
-        if (n == 1)
-        {
-            insert_symbol_sup(user_in, cur_pos, c, 2);
-            return ;
-        }
-        insert_symbol_sup(user_in, cur_pos, c, 0);
-    }
+    ret_cur_to_original_pos(user_in, cur_pos);
 }
