eed5e17020709e24fed116640e4685039d3efb57
anton <goloviznin1995@yandex.ru>
2020-05-04 08:48:44 +0300

    Parser "eats" infinite amounts of \n before command.
    Program eats all the ram and crashes (lexer probably does not
    require the next line from readline)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -8,9 +8,15 @@ t_deque	*parser()
 {
 	t_deque			*command;
 	static t_deque	*tokbuf_g;
+	t_deque			*tokbuf_l;
 
 	command = NULL;
 	tokbuf_g = NULL;
+	tokbuf_l = NULL;
+	while (gett(&tokbuf_g, &tokbuf_l)->token_type == __newline)
+		;
+	ungett(&tokbuf_g, &tokbuf_l);
+	erase_tokbuf(&tokbuf_l);
 	if (PARSER_FAILURE == match_complete_command(&command, &tokbuf_g))
 	{
 		rm_compl_cmd(&command);
