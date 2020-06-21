e715f205391f5f54ffcf2556f4ba0db1f7add89a
user <Artur.Tsorionov@advalange.com>
2020-06-21 14:46:39 +0300

    Привел в норму str_n

---
+++ src/readline/str_n.c
@@ -0,0 +1,46 @@
+#include "21sh.h"
+
+static int	str_n_sup(char *user_in, int n)
+{
+	unsigned short	cur_pos_col;
+
+	cur_pos_col = 1;
+	while (*user_in)
+	{
+		if (*user_in == '\n' || cur_pos_col >= rp()->ws_col)
+		{
+			cur_pos_col = 0;
+			n++;
+		}
+		cur_pos_col++;
+		user_in++;
+	}
+	return (n);
+}
+
+/*
+** Возвращает количество строк, занимаемых в терминале,
+** командой (rp()->user_in).
+*/
+
+int			str_n(void)
+{
+	char			*user_in;
+	int				n;
+	unsigned short	cur_pos_col;
+
+	user_in = rp()->user_in;
+	n = 0;
+	cur_pos_col = rp()->prompt_len;
+	while (*user_in && *user_in != '\n' && cur_pos_col <= rp()->ws_col)
+	{
+		cur_pos_col++;
+		user_in++;
+	}
+	if ((*user_in == '\n' || cur_pos_col > rp()->ws_col) && *user_in)
+	{
+		n++;
+		user_in++;
+	}
+	return (str_n_sup(user_in, n));
+}
