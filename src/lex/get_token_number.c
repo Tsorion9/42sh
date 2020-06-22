1c2687324669f6859ce900c0bbb7fc14b3fbbe22
user <Artur.Tsorionov@advalange.com>
2020-06-22 21:51:00 +0300

    [NORME] get_token_number

--- src/lex/get_token_number.c
+++ src/lex/get_token_number.c
@@ -1,26 +1,26 @@
 #include "lex.h"
 
-t_token       get_token_number(char **user_in, int *index, t_str *attr, \
+t_token	get_token_number(char **user_in, int *index, t_str *attr, \
 	int prev_token)
 {
-    t_token   ret_token;
+	t_token	ret_token;
 
-    while (is_digit((*user_in)[*index]))
-        write_char_to_buf(*user_in, index, attr);
-    if ((*user_in)[*index] == '>' || (*user_in)[*index] == '<')
-    {
-        ret_token.token_type = NUMBER;
-        ret_token.attribute = create_attribute(attr);
-    }
-    else if (is_letter((*user_in)[*index]) || (*user_in)[*index] == '\'' ||\
-    (*user_in)[*index] == '\"')
-        ret_token = get_token_word(user_in, index, attr);
-    else if (prev_token == LESS_AND || prev_token == GREATER_AND)
-    {
-        ret_token.token_type = NUMBER;
-        ret_token.attribute = create_attribute(attr);
-    }
-    else
-        ret_token = get_token_word(user_in, index, attr);
-    return (ret_token);
+	while (is_digit((*user_in)[*index]))
+		write_char_to_buf(*user_in, index, attr);
+	if ((*user_in)[*index] == '>' || (*user_in)[*index] == '<')
+	{
+		ret_token.token_type = NUMBER;
+		ret_token.attribute = create_attribute(attr);
+	}
+	else if (is_letter((*user_in)[*index]) || (*user_in)[*index] == '\'' ||\
+	(*user_in)[*index] == '\"')
+		ret_token = get_token_word(user_in, index, attr);
+	else if (prev_token == LESS_AND || prev_token == GREATER_AND)
+	{
+		ret_token.token_type = NUMBER;
+		ret_token.attribute = create_attribute(attr);
+	}
+	else
+		ret_token = get_token_word(user_in, index, attr);
+	return (ret_token);
 }
