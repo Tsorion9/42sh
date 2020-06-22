e30ce43f7fe3ee7cef4b18d5971047d6f2664bc8
user <Artur.Tsorionov@advalange.com>
2020-06-22 21:57:27 +0300

    [NORME] write_quotes_to_buf

--- src/lex/write_quotes_to_buf.c
+++ src/lex/write_quotes_to_buf.c
@@ -1,23 +1,23 @@
 #include "lex.h"
 
-t_token       write_singe_quotes_to_buf(char **user_in, int *index, t_str *attr)
+t_token	write_singe_quotes_to_buf(char **user_in, int *index, t_str *attr)
 {
-    write_char_to_buf(*user_in, index, attr);
-    while ((*user_in)[*index] != '\'')
-        write_char_to_buf(*user_in, index, attr);
-    write_char_to_buf(*user_in, index, attr);
-    return (get_token_word(user_in, index, attr));
+	write_char_to_buf(*user_in, index, attr);
+	while ((*user_in)[*index] != '\'')
+		write_char_to_buf(*user_in, index, attr);
+	write_char_to_buf(*user_in, index, attr);
+	return (get_token_word(user_in, index, attr));
 }
 
-t_token       write_double_quotes_to_buf(char **user_in, int *index, t_str *attr)
+t_token	write_double_quotes_to_buf(char **user_in, int *index, t_str *attr)
 {
-    write_char_to_buf(*user_in, index, attr);
-    while (1)
-    {
-        if ((*user_in)[*index] == '\"' && check_slash(*user_in, *index - 1))
-            break ;
-        write_char_to_buf(*user_in, index, attr);
-    }
-    write_char_to_buf(*user_in, index, attr);
-    return (get_token_word(user_in, index, attr));
+	write_char_to_buf(*user_in, index, attr);
+	while (1)
+	{
+		if ((*user_in)[*index] == '\"' && check_slash(*user_in, *index - 1))
+			break ;
+		write_char_to_buf(*user_in, index, attr);
+	}
+	write_char_to_buf(*user_in, index, attr);
+	return (get_token_word(user_in, index, attr));
 }
