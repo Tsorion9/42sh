828830515df831ed2f8995ffac0f1b8d87fdb4e2
user <Artur.Tsorionov@advalange.com>
2020-06-22 20:46:07 +0300

    [NORME] clear_all_line

--- src/readline/clear_all_line.c
+++ src/readline/clear_all_line.c
@@ -4,10 +4,10 @@
 ** Очищает строку.
 */
 
-static void clear_line(int end_cl)
+static void	clear_line(int end_cl)
 {
-    while (rp()->cur_pos[0] > end_cl)
-		  tc_cursor_left();
+	while (rp()->cur_pos[0] > end_cl)
+		tc_cursor_left();
 	tc_clear_till_end();
 }
 
@@ -15,26 +15,26 @@ static void clear_line(int end_cl)
 ** Чистит все строки, введенные пользователем.
 */
 
-void        clear_all_line(void)
+void		clear_all_line(void)
 {
-    int n;
+	int n;
 
-    n = str_n() + 1;
-    if (n == 1)
-    {
-        clear_line(rp()->prompt_len);
-        rp()->cur_pos[0] = rp()->prompt_len;
-        return ;
-    }
-    while (rp()->cur_pos[0] != 1)
-        tc_cursor_left();
-    while (rp()->cur_pos[1] != n)
-        tc_cursor_down();
-    while (rp()->cur_pos[1] > 1)
-    {
-        clear_line(0);
-        tc_cursor_up();
-    }
+	n = str_n() + 1;
+	if (n == 1)
+	{
+		clear_line(rp()->prompt_len);
+		rp()->cur_pos[0] = rp()->prompt_len;
+		return ;
+	}
+	while (rp()->cur_pos[0] != 1)
+		tc_cursor_left();
+	while (rp()->cur_pos[1] != n)
+		tc_cursor_down();
+	while (rp()->cur_pos[1] > 1)
+	{
+		clear_line(0);
+		tc_cursor_up();
+	}
 	tc_cursor_n_right(rp()->prompt_len - 1);
-    tc_clear_till_end();
+	tc_clear_till_end();
 }
