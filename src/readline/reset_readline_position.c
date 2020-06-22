9817d91fbad3e2e9e3821b2d56592fc8c31cc801
user <Artur.Tsorionov@advalange.com>
2020-06-22 20:40:24 +0300

    [NORME] reset_readline_position

--- src/readline/reset_readline_position.c
+++ src/readline/reset_readline_position.c
@@ -1,6 +1,6 @@
 #include "21sh.h"
 
-static void back_to_start_history_rp(void)
+static void	back_to_start_history_rp(void)
 {
 	if (rp()->history)
 		while (rp()->history->prev)
@@ -13,13 +13,13 @@ static void	reset_cur_pos_rp(void)
 	rp()->cur_pos[1] = 1;
 }
 
-void		reset_rp_to_start(char *_prompt)
+void		reset_rp_to_start(char *new_prompt)
 {
 	if (!(rp()->user_in = (char*)ft_memalloc(sizeof(char) * MIN_CMD_LENGTH)))
 		reset_exit(1);
 	rp()->max_len = MIN_CMD_LENGTH;
 	rp()->len = 0;
 	back_to_start_history_rp();
-	rp()->prompt_len = ft_strlen(_prompt) + 1;
+	rp()->prompt_len = ft_strlen(new_prompt) + 1;
 	reset_cur_pos_rp();
 }
