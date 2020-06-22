596ef9cdff888dea05944a851cae3dab04559fe0
user <Artur.Tsorionov@advalange.com>
2020-06-22 20:42:00 +0300

    [NORME] free_readline

--- src/readline/free_readline.c
+++ src/readline/free_readline.c
@@ -1,22 +1,22 @@
 #include "21sh.h"
 
-void		free_history_list(t_history *history)
+void	free_history_list(t_history *history)
 {
-    if (!history)
-        return ;
-    while (history->prev)
-        history = history->prev;
-    while (history->next)
-    {
-        history = history->next;
-        free(history->prev->str);
-        free(history->prev);
-    }
-    free(history->str);
-    free(history);
+	if (!history)
+		return ;
+	while (history->prev)
+		history = history->prev;
+	while (history->next)
+	{
+		history = history->next;
+		free(history->prev->str);
+		free(history->prev);
+	}
+	free(history->str);
+	free(history);
 }
 
-void        free_readline_position(void)
+void	free_readline_position(void)
 {
 	free(rp()->user_in);
 	free_history_list(rp()->history);
