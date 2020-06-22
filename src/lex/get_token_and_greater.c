f44ce7745c49fe9898a49c4d4cf704499922d2ca
user <Artur.Tsorionov@advalange.com>
2020-06-22 21:46:18 +0300

    [NORME] get_token_and_greater

--- src/lex/get_token_and_greater.c
+++ src/lex/get_token_and_greater.c
@@ -1,18 +1,18 @@
 #include "lex.h"
 
-t_token       get_token_and_greater(char **user_in, int *index, t_str *attr)
+t_token	get_token_and_greater(char **user_in, int *index, t_str *attr)
 {
-    t_token   ret_token;
+	t_token	ret_token;
 
-    if ((*user_in)[*index + 1] == '>')
-    {
-        ret_token.token_type = AND_GREATOR;
-        (*index) += 2;
-    }
-    else
-    {
-        write_char_to_buf(*user_in, index, attr);
-        ret_token = get_token_word(user_in, index, attr);
-    }
-    return (ret_token);
+	if ((*user_in)[*index + 1] == '>')
+	{
+		ret_token.token_type = AND_GREATOR;
+		(*index) += 2;
+	}
+	else
+	{
+		write_char_to_buf(*user_in, index, attr);
+		ret_token = get_token_word(user_in, index, attr);
+	}
+	return (ret_token);
 }
