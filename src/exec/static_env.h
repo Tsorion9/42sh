19ec5ca279de52901ff9273098223e7b9fccc975
anton <goloviznin1995@yandex.ru>
2020-06-23 02:05:18 +0300

    [NORME] exec *.h normed

--- src/exec/static_env.h
+++ src/exec/static_env.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   static_env.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anton <a@b>                                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 02:02:56 by anton             #+#    #+#             */
+/*   Updated: 2020/06/23 02:02:56 by anton            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STATIC_ENV_H
 # define STATIC_ENV_H
 
@@ -7,12 +19,11 @@ typedef enum	e_env_action
 {
 	init,
 	get,
-	save,		/* For subshell. Makes static copy of original env */
-	restore,	/* Swaps copy and original. Deletes modified original*/
+	save,
+	restore,
 	del
 }				t_env_action;
 
 t_env	static_env_action(t_env_action action, void *opt_arg);
-
 
 #endif
