94a1ef5ca8f9f7bc9a695a119d6e30a289c675ef
user <Artur.Tsorionov@advalange.com>
2020-06-22 21:50:06 +0300

    [NORME] get_token_line_separator

--- src/lex/get_token_line_separator.c
+++ src/lex/get_token_line_separator.c
@@ -1,10 +1,10 @@
 #include "lex.h"
 
-t_token       get_toket_line_separator(int *index)
+t_token	get_toket_line_separator(int *index)
 {
-    t_token   ret_token;
+	t_token	ret_token;
 
-    ret_token.token_type = LINE_SEPARATOR;
-    (*index)++;
-    return (ret_token);
+	ret_token.token_type = LINE_SEPARATOR;
+	(*index)++;
+	return (ret_token);
 }
