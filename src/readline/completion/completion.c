146766d97e87d33ffac235dd6b03280e43884e88
Alex Buyanov <alexbuyanov@nriker.local>
2020-10-18 18:27:54 +0300

    Добавлена обработка >&

--- src/readline/completion/completion.c
+++ src/readline/completion/completion.c
@@ -6,7 +6,7 @@
 /*   By: alexbuyanov <alexbuyanov@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/23 18:11:18 by mphobos           #+#    #+#             */
-/*   Updated: 2020/10/17 18:25:29 by alexbuyanov      ###   ########.fr       */
+/*   Updated: 2020/10/18 18:24:59 by alexbuyanov      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,25 @@
 // str_n(void) - возвращает количество строк занимаемых командой
 // rp() - получение статической структуры позиции курсора
 
+int			check_arrow_amper(char *user_in, int i)
+{
+	if (user_in[i] == '\0')
+		i--;
+	while (i && user_in[i] != ' ' && user_in[i] != '\t' && \
+		user_in[i] != '&')
+		i--;
+	if (i && user_in[i] == '&' && user_in[i - 1] == '>')
+		return (0);
+	return (1);
+}
+
+int			check_space_or_tab(char *user_in, int i)
+{
+	if (user_in[i] == ' ' || user_in[i] == '\t')
+		return (1);
+	return (0);
+}
+
 int			check_first_word(char *user_in, int i)
 {
 	int		j;
@@ -25,18 +44,41 @@ int			check_first_word(char *user_in, int i)
 	j = 0;
 	if (!ft_strlen(user_in))
 		return (1);
-	while (user_in[j] == ' ' || user_in[j] == '\t')
+	else if (!check_arrow_amper(user_in, i))
+		return (0);
+	while (check_space_or_tab(user_in, j))
 		j++;
+	// while(1);
 	while (j < i)
 	{
-		if ((user_in[j] == '\t' || user_in[j] == ' ')
+		if (check_space_or_tab(user_in, j)
 			&& j == i)
+		{
+			while (!check_space_or_tab(user_in, i) && \
+				user_in[i] != '&')
+			{
+				i--;
+			}
+			if (i > 0 && user_in[i] == '&' && user_in[i - 1] == '>')
+				return (0);
 			return (1);
-		else if ((user_in[j] == '\t' || user_in[j] == ' '))
+		}
+		else if (check_space_or_tab(user_in, j))
 			return (0);
 		j++;
 	}
+		// while (1);
 	return (1);
+}
+
+int			tab_check_special_symbols(char *user_in, int j)
+{
+	if (j > 0 && (user_in[j]  == '|' || (user_in[j] == '&' && \
+		user_in[j - 1] != '>')))
+	{
+		return (1);
+	}
+	return (0);
 }
 
 static int	is_first_word(char *user_in, int i)
@@ -45,23 +87,31 @@ static int	is_first_word(char *user_in, int i)
 
 	j = i;
 	if (check_first_word(user_in, i))
+	{
 		return (1);
-	while (j >= 0 && user_in[j] != '|' && user_in[j] != '&')
+	}
+	// while (j >= 0 && user_in[j] != '|' && user_in[j] != '&')
+	while (j > 0 && !tab_check_special_symbols(user_in, j))
+	{
+		if (user_in[j] == '&' && user_in[j - 1] == '>')
+			return (0);
+		// write(1, "t", 1);
 		j--;
+	}
 	if (j >= 0 && (user_in[j] == '|' || user_in[j] == '&'))
 	{
-		while (user_in[j] == ' ' || user_in[j] == '\t' || \
+		while (check_space_or_tab(user_in, i) || \
 			user_in[j] == '|' || user_in[j] == '&')
 			j++;
 		// while (1);
 			// j++;
 		while (j < i)
 		{
-			if ((user_in[j] == '\t' || user_in[j] == ' ' || user_in[j] == '|' \
+			if ((check_space_or_tab(user_in, i) || user_in[j] == '|' \
 				|| user_in[j] == '\0' || user_in[j] == '&') && j == i)
 				return (2);
-			else if (user_in[j] == '\t' || user_in[j] == ' ' \
-				/*|| user_in[j] == '|'*/ || user_in[j] == '\0')
+			else if (check_space_or_tab(user_in, i) \
+				|| user_in[j] == '\0')
 				return (0);
 			// ft_printf("\n%d %d", j, i);
 			// while (1);
