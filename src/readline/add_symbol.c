a3ac609649b25bc22246c5b20fc586a609e8efd5
tsorion <tsorion_9@mail.ru>
2020-12-19 14:04:27 +0300

    Исправлен баг с выводом новой строки, когда курсор остается на предыдушей

--- src/readline/add_symbol.c
+++ src/readline/add_symbol.c
@@ -6,7 +6,7 @@
 /*   By: mphobos <mphobos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 13:18:00 by mphobos           #+#    #+#             */
-/*   Updated: 2020/12/01 22:06:44 by mphobos          ###   ########.fr       */
+/*   Updated: 2020/12/19 13:58:39 by mphobos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ void	add_symbol_in_str(char *str, char symbol, size_t symbol_index)
 
 void	add_symbol(char c)
 {
+	int cur_pos[2];
+
 	if (rp(NULL)->max_len <= rp(NULL)->len + 1)
 		expand_user_in();
 	add_symbol_in_str(rp(NULL)->user_in, c, rp(NULL)->index);
@@ -33,5 +35,8 @@ void	add_symbol(char c)
 	tc_save_cursor_pos();
 	ft_putstr_fd(rp(NULL)->user_in + rp(NULL)->index, STDERR_FILENO);
 	tc_restore_saved_cursor_pos();
+	cur_pos_after_putstr(cur_pos, rp(NULL)->prompt_len);
 	move_cursor(RIGHT_ARROW);
+	if (cur_pos[0] == 2 && rp(NULL)->index != rp(NULL)->len && rp(NULL)->cur_pos[0] != 1)
+		tputs(tgetstr("up", NULL), STDERR_FILENO, ft_putint);
 }
