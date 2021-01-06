c1ba0e906cccff180efbef94bbaafaa627df68d5
soliddsnake <alex.d.buyanov@gmail.com>
2021-01-07 00:05:01 +0300

    Исправлен alias

--- src/exec/builtin_alias.c
+++ src/exec/builtin_alias.c
@@ -6,7 +6,7 @@
 /*   By: nriker <nriker@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/28 11:34:50 by anton             #+#    #+#             */
-/*   Updated: 2021/01/05 13:37:42 by nriker           ###   ########.fr       */
+/*   Updated: 2021/01/07 00:04:33 by nriker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int			builtin_alias(char **args, t_env env, int subshell)
 	else
 		while (*args)
 		{
-			if (*args && args[1] == NULL)
+			if (*args && (args[1] == NULL || ft_strcmp(args[1], "=")))
 				alias_api_print_alias(*args);
 			else if (*args && !ft_strcmp(args[1], "="))
 				alias_api_insert_alias(alias, *args, args[2]);
