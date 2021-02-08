9f802edd7a938af9a4ceab2250196c50c05cfdf6
tsorion <tsorion_9@mail.ru>
2021-02-08 13:28:23 +0300

    [EXPANSIONS] add expansions !

--- src/expansions/word_expansion.c
+++ src/expansions/word_expansion.c
@@ -2,6 +2,7 @@
 #include "expansions.h"
 #include "environment.h"
 #include "exec.h"
+#include "readline.h"
 
 /*
 ** На вход ожидает получить строку с кавычкой в первом символе
@@ -503,6 +504,118 @@ void 	dollar_expansion(char **src_word, size_t *i, int *word_state)
 		(*i)++;
 }
 
+static void			init_history_num_and_first(t_history **history_first,
+	int *number_of_history)
+{
+	t_history *history;
+
+	history = rp(NULL)->history;
+	while (history->prev)
+		history = history->prev;
+	*number_of_history = 0;
+	while (history->next)
+	{
+		history = history->next;
+		(*number_of_history)++;
+	}
+	*history_first = history;
+	(*number_of_history)--;
+}
+
+static t_history	*get_history(t_history *history_first,
+	const int number_of_history, int history_number)
+{
+	t_history *history;
+
+	if (history_number > number_of_history)
+		history_number = number_of_history - 1;
+	if (history_number < 0)
+	{
+		history_number = number_of_history + history_number;
+		if (history_number < 1)
+			history_number = 1;
+	}
+	history = history_first;
+	while (history->prev && history_number)
+	{
+		history = history->prev;
+		history_number--;
+	}
+	return (history);
+}
+
+static int			search_str_in_history(const char *str)
+{
+	t_history	*history;
+	int			found;
+	int			history_number;
+
+	history_number = 0;
+	history = rp(NULL)->history;
+	while (history->prev)
+		history = history->prev;
+	found = 0;
+	while (history && !found && history_number < HISTSIZE)
+	{
+		if (ft_strfirststr(history->str, str))
+			found = 1;
+		history = history->next;
+		history_number++;
+	}
+	return ((history_number - 2) * (-1));
+}
+
+static int			search_history_number(char *str)
+{
+	if (str)
+	{
+		if (ft_isnumber(str))
+			return (ft_atoi(str));
+		else if (str[0] != '-')
+			return (search_str_in_history(str));
+	}
+	return (-1);
+}
+
+static size_t		find_end_history_expansions(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (ft_isalpha(*str) || *str == '-' || ft_isdigit(*str))
+	{
+		str++;
+		len++;
+	}
+	return (len);
+}
+
+void 				history_expansion(char **src_word, size_t *i)
+{
+	char		c;
+	t_history	*history_first;
+	int			number_of_history;
+	t_history	*history;
+	int			history_number;
+
+	c = (*src_word)[*i + 1];
+	init_history_num_and_first(&history_first, &number_of_history);
+	if (c == '!')
+	{
+		history = get_history(history_first, number_of_history, -1);
+		replace_value(src_word, history->str, i, 2);
+	}
+	else if (ft_isalpha(c) || c == '-' || ft_isdigit(c))
+	{
+		history_number = search_history_number(*src_word + 1);
+		history = get_history(history_first, number_of_history, history_number);
+		replace_value(src_word, history->str, i, 
+			find_end_history_expansions(*src_word) - 1);
+	}
+	else
+		(*i)++;
+}
+
 /*
 ** source_word malloced
 */
@@ -543,6 +656,8 @@ int		word_expansion(char **source_word)
 		}
 		else if (c == '$')
 			dollar_expansion(source_word, &i, &word_state);
+		else if (c == '!')
+			history_expansion(source_word, &i);
 		else
 			i++;
 	}
