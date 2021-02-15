0fd9b5a5f3098a694a77dac67862f3d5f9071f69
soliddsnake <alex.d.buyanov@gmail.com>
2021-02-15 07:13:24 +0300

    Исправлен alias

--- src/exec/builtin_alias.c
+++ src/exec/builtin_alias.c
@@ -6,11 +6,41 @@
 /*   By: nriker <nriker@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/28 11:34:50 by anton             #+#    #+#             */
-/*   Updated: 2021/02/14 00:13:33 by nriker           ###   ########.fr       */
+/*   Updated: 2021/02/15 07:11:59 by nriker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "t_hashalias.h"
+
+void		check_previous_keys(char *key, char* value)
+{
+	t_hashalias		*hash_alias;
+	t_hashtable		*table;
+	int				i;
+
+	if ((hash_alias = static_hashalias_action(get)) == NULL
+		|| hash_alias->hd == NULL)
+		return ;
+	i = 0;
+	while (i < HASH_SIZE)
+	{
+		table = hash_alias->hd->hashtable[i];
+		if (table)
+		{
+			while (table)
+			{
+				if (!ft_strcmp(table->value, key))
+				{
+					insert_alias(table->key, value);
+				}
+				// delete_alias
+				// ft_printf("alias %s='%s'\n", table->key, table->value);
+				table = table->next;
+			}
+		}
+		i++;
+	}
+}
 
 void		check_alias(char *arg)
 {
@@ -40,6 +70,7 @@ void		builtin_alias_cycle_args(char **mas_args)
 		else if (mas_args[i] && !ft_strcmp(mas_args[i + 1], "="))
 		{
 			insert_alias(mas_args[i], mas_args[i + 2]);
+			check_previous_keys(mas_args[i], mas_args[i + 2]);
 			i += 3;
 		}
 	}
@@ -58,7 +89,7 @@ int			builtin_alias(char **args, t_env env, int subshell)
 	{
 		if (check_flag_p(&args, print_all_aliases, invalid_alias_option) == EXIT_FAILURE)
 			return (EXIT_FAILURE);
-		if ((mas_args = get_alias_args(args,invalid_alias_name)) == NULL)
+		if ((mas_args = get_alias_args(args, invalid_alias_name)) == NULL)
 			return (EXIT_FAILURE);
 		builtin_alias_cycle_args(mas_args);
 		del_array(mas_args);
