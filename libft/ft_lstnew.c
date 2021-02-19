7e9d6636fbe8e1862371d7a6daa8b997c891d82c
Anton <goloviznin1995@yandex.ru>
2021-02-20 02:06:30 +0300

    [LIBFT] Use Arthur's lstnew

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -3,24 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgian <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: mphobos <mphobos@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/23 16:41:31 by bgian             #+#    #+#             */
-/*   Updated: 2019/09/23 16:43:38 by bgian            ###   ########.fr       */
+/*   Created: 2019/09/11 19:15:46 by mphobos           #+#    #+#             */
+/*   Updated: 2019/10/12 15:12:00 by mphobos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void const *content, size_t content_size)
+t_list	*ft_lstnew(const void *content, size_t content_size)
 {
-	t_list	*l;
+	t_list	*ptr;
 
-	l = (t_list *)malloc(sizeof(t_list));
-	if (!l)
-		return (0);
-	l->next = 0;
-	l->content = (void *)content;
-	l->content_size = content ? content_size : 0;
-	return (l);
+	if (!(ptr = (t_list*)xmalloc(sizeof(t_list) * 1)))
+		return (NULL);
+	ptr->next = NULL;
+	if (content == NULL)
+	{
+		ptr->content = NULL;
+		ptr->content_size = 0;
+		return (ptr);
+	}
+	if (!(ptr->content = (void*)xmalloc(content_size)))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ft_memcpy(ptr->content, content, content_size);
+	ptr->content_size = content_size;
+	return (ptr);
 }
