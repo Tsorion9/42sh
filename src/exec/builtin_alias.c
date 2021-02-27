f1d3e620a845bcf372ce0d9d4f8cce15bc5e2248
soliddsnake <alex.d.buyanov@gmail.com>
2021-02-27 22:22:11 +0300

    [ALIAS][#73][Проверка на валидность названий alias]

--- src/exec/builtin_alias.c
+++ src/exec/builtin_alias.c
@@ -6,7 +6,7 @@
 /*   By: nriker <nriker@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/28 11:34:50 by anton             #+#    #+#             */
-/*   Updated: 2021/02/24 21:17:39 by nriker           ###   ########.fr       */
+/*   Updated: 2021/02/27 22:19:46 by nriker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,23 @@ void		check_alias(char *arg)
 	}
 }
 
-void		builtin_alias_cycle_args(char **mas_args)
+int			check_valid_alias_name(char *name)
+{
+	int		i;
+
+	i = 0;
+	while (name[i])
+	{
+		if (!ft_isalpha(name[i]) && !ft_isalnum(name[i]) && name[i] != '_'
+			&& name[i] != '!' && name[i] != '%' && name[i] != ','
+			&& name[i] != '@' && name[i] != '-')
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int			builtin_alias_cycle_args(char **mas_args)
 {
 	int		i;
 
@@ -39,10 +55,16 @@ void		builtin_alias_cycle_args(char **mas_args)
 			check_alias(mas_args[i++]);
 		else if (mas_args[i] && !ft_strcmp(mas_args[i + 1], "="))
 		{
+			if (check_valid_alias_name(mas_args[i]) == EXIT_FAILURE)
+			{
+				invalid_alias_name(mas_args[i]);
+				return (EXIT_FAILURE);
+			}
 			insert_alias(mas_args[i], mas_args[i + 2]);
 			i += 3;
 		}
 	}
+	return (EXIT_SUCCESS);
 }
 
 int			builtin_alias(char **args, t_env env, int subshell)
@@ -56,11 +78,10 @@ int			builtin_alias(char **args, t_env env, int subshell)
 		print_all_aliases();
 	else
 	{
-		if (check_flag_p(&args, print_all_aliases, invalid_alias_option) == EXIT_FAILURE)
+		if ((check_flag_p(&args, print_all_aliases, invalid_alias_option) == EXIT_FAILURE)
+			|| ((mas_args = get_alias_args(args, invalid_alias_name)) == NULL)
+			|| (builtin_alias_cycle_args(mas_args) == EXIT_FAILURE))
 			return (EXIT_FAILURE);
-		if ((mas_args = get_alias_args(args, invalid_alias_name)) == NULL)
-			return (EXIT_FAILURE);
-		builtin_alias_cycle_args(mas_args);
 		del_array(mas_args);
 	}
 	return (EXIT_SUCCESS);
