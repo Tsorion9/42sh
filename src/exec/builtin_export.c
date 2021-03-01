60dd5553f28bc6f6518e0430dcb665b7a48b110b
soliddsnake <alex.d.buyanov@gmail.com>
2021-03-02 00:41:23 +0300

    [EXPORT][Улучшена обработка невалидных имен переменных. Norminette builtin_export.c]

--- src/exec/builtin_export.c
+++ src/exec/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: nriker <nriker@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/10 14:52:20 by nriker            #+#    #+#             */
-/*   Updated: 2021/03/01 23:14:28 by nriker           ###   ########.fr       */
+/*   Updated: 2021/03/02 00:37:43 by nriker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,22 +36,47 @@ int			check_valid_flag_export(char *arg, void (*print)(char *arg))
 	return (EXIT_SUCCESS);
 }
 
+int			check_valid_export_name(char *name)
+{
+	char		*key;
+	int			i;
+
+	i = 0;
+	if (ft_strchr(name, '='))
+		key = ft_strcut(name, '=');
+	else
+		key = ft_strdup(name);
+	if (!ft_isalpha(key[i]) && key[i] != '_')
+	{
+		free(key);
+		ft_fprintf(2, "42sh: export: '%s': not a valid identifier\n", key);
+		return (EXIT_FAILURE);
+	}
+	while (key[++i])
+	{
+		if (!ft_isdigit(key[i]) && !ft_isalpha(key[i]) && key[i] != '_')
+		{
+			free(key);
+			ft_fprintf(2, "42sh: export: '%s': not a valid identifier\n", key);
+			return (EXIT_FAILURE);
+		}
+	}
+	free(key);
+	return (EXIT_SUCCESS);
+}
+
 static int	handle_export_arg(char *arg)
 {
 	char	*equal;
-	char	*value;
 	char	*key;
+	char	*value;
 
 	key = NULL;
+	if (check_valid_export_name(arg) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
 	if ((equal = ft_strchr(arg, '=')))
 	{
 		key = ft_strcut(arg, '=');
-		if (ft_isdigit(*key))
-		{
-			free(key);
-			ft_fprintf(STDERR_FILENO, "42sh: export: bad arg'%s': \n", arg);
-			return (EXIT_FAILURE);
-		}
 		ft_setenv(g_export_env, key, ft_strdup(equal + 1));
 		ft_setenv(env, key, ft_strdup(equal + 1));
 		free(key);
@@ -61,6 +86,8 @@ static int	handle_export_arg(char *arg)
 		value = ft_getenv(env, arg);
 		if (value)
 			ft_setenv(g_export_env, arg, ft_strdup(value));
+		else
+			ft_setenv(g_export_env, arg, ft_strdup(""));
 	}
 	return (EXIT_SUCCESS);
 }
