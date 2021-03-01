c3526816d927c6601c25f8038f6f9cd42bc42810
soliddsnake <alex.d.buyanov@gmail.com>
2021-03-01 22:01:34 +0300

    Norminette hash: static_hash_ection.c, t_hash.h

--- src/exec/static_hash_action.c
+++ src/exec/static_hash_action.c
@@ -6,7 +6,7 @@
 /*   By: nriker <nriker@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 06:45:56 by nriker            #+#    #+#             */
-/*   Updated: 2021/02/22 18:19:32 by nriker           ###   ########.fr       */
+/*   Updated: 2021/03/01 21:57:58 by nriker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,20 @@
 t_hash			*static_hash_action(int action)
 {
 	static t_hash	*hash;
-	// char			*env_path;
 
 	if (action == init)
 	{
 		if ((hash = init_t_hash()) == NULL)
-			return NULL;
-		// env_path = ft_getenv(env, "PATH");
+			return (NULL);
 		hash->copy_path = ft_strsplit(ft_getenv(env, "PATH"), ':');
 		return (hash);
 	}
 	else if (action == get)
-		return hash;
+		return (hash);
 	else if (action == del)
 	{
 		delete_t_hash();
-		hash = NULL;
+		hash = (NULL);
 	}
 	return (NULL);
 }
