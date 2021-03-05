fb411a24d102fa2760301e36d078309d58329163
volkker <volkov2605000@mail.ru>
2021-03-05 23:29:49 +0300

    [ALIAS][Remove usused code]

--- src/parser/match_simple_command.c
+++ src/parser/match_simple_command.c
@@ -11,8 +11,6 @@
 /* ************************************************************************** */
 
 #include "parser.h"
-#include "t_hashalias.h"
-#include "lexer.h"
 
 t_simple_cmd	*init_simple_cmd(void)
 {
@@ -20,104 +18,6 @@ t_simple_cmd	*init_simple_cmd(void)
 
 	fresh = (t_simple_cmd*)ft_memalloc(sizeof(t_simple_cmd));
 	return (fresh);
-}
-
-void			print_token(t_token token)
-{
-	ft_printf("%s\n", token.value);
-	ft_printf("%s\n", get_token_str(token.tk_type));
-}
-
-void			print_tokbuf(t_token *token)
-{
-	print_token(*token);
-}
-
-void			substitute_alias(t_token *token, t_deque **tokbuf_g)
-{
-	char		*value;
-	t_hashtable	*data;
-	t_deque		*tokbuf_l;
-	t_deque		*first_alias_tokbuf;
-	t_deque		*tokbuf_fresh;
-	int			need_expand_next_alias;
-	int			j;
-	int			not_empty_links;
-	t_token		*tmp;
-	t_token		*del;
-
-	data = NULL;
-	tokbuf_fresh = NULL;
-	tokbuf_l = NULL;
-	first_alias_tokbuf = NULL;
-	not_empty_links = 0;
-	need_expand_next_alias = 1;
-	 while (need_expand_next_alias)
-	 {
-		tmp = gett(g_parser_input_str, tokbuf_g, &tokbuf_l);
-		need_expand_next_alias = 0;
-		data = search_alias_hash_element(tmp->value);
-		if (data)
-			need_expand_next_alias = data->expand_next_alias;
-		if (tmp->tk_type == WORD && !tmp->do_not_expand_alias)
-		{
-			// 1-й токен из значения алиаса
-			value = search_alias_1(tmp->value, &j);
-			// TODO Remove this if, it's unused, need remove j
-			if (!value && j) {
-				free(tmp->value);
-				free(tmp);
-				return ;
-			}
-			// Нет алиаса или токен не имеет строкового представления
-			if (value == NULL)
-			{
-				// Если нет строкового представления токена, то добавляет в
-				// tokbuf_fresh буфер токенов значения алиаса
-				// Иначе кладем токен в tokbuf_fresh, значит это не алиас
-				non_string_alias(tmp, &tokbuf_fresh);
-			}
-			else
-			{
-				// Есть строковое представление токена
-				// Сохраняем буфер значения первого алиаса, без первого токена
-				first_alias_tokbuf = deque_copy(search_tokbuf(tmp->value));
-				// Чистка памяти
-				del = pop_front(first_alias_tokbuf);
-				free(del->value);
-				free(del);
-				// заполняем связки
-				value_of_token_is_not_null(tmp, &tokbuf_fresh, &not_empty_links);
-				free(value);
-				// Защита от зацикливания
-				set_null_meet_alias();
-				// Чистка памяти
-				free(tmp->value);
-				free(tmp);
-			}
-			// Кладем буфер первого алиаса в tokbuf_fresh
-			if (not_empty_links)
-				flush_tokbuf_back(&tokbuf_fresh, first_alias_tokbuf);
-			else
-				erase_tokbuf(&first_alias_tokbuf);
-			// Кладем остаток токенов глобального буфера в tokbuf_fresh
-			flush_tokbuf_back(&tokbuf_fresh, *tokbuf_g);
-			// Устанавливаем в глобальный буфер tokbuf_fresh
-		}
-		else
-			push_front(&tokbuf_fresh, tmp);
-	 }
-	*tokbuf_g = tokbuf_fresh;
-}
-
-static void		check_alias(t_token **token, t_deque **tokbuf_g,
-							t_deque **tokbuf_l)
-{
-	*token = gett(g_parser_input_str, tokbuf_g, tokbuf_l);
-	ungett(tokbuf_g, tokbuf_l);
-	substitute_alias(*token, tokbuf_g);
-	*token = gett(g_parser_input_str, tokbuf_g, tokbuf_l);
-	ungett(tokbuf_g, tokbuf_l);
 }
 
 int				match_simple_command(t_simple_cmd **simple_cmd,
@@ -130,7 +30,6 @@ int				match_simple_command(t_simple_cmd **simple_cmd,
 	*simple_cmd = init_simple_cmd();
 	token = gett(g_parser_input_str, tokbuf_g, &tokbuf_l);
 	ungett(tokbuf_g, &tokbuf_l);
-//	check_alias(&token, tokbuf_g, &tokbuf_l);
 	if (token->tk_type == WORD || token->tk_type == IO_NUMBER
 		|| is_redirect(token->tk_type))
 	{
