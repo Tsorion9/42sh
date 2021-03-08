89ecc78a130aadc1b7d77989938454e95f86487b
soliddsnake <alex.d.buyanov@gmail.com>
2021-03-08 10:13:57 +0300

    [HASH][Добавлен флаг -d для удаления отдельных элементов hash]

--- src/exec/builtin_hash.c
+++ src/exec/builtin_hash.c
@@ -6,7 +6,7 @@
 /*   By: nriker <nriker@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/09 23:41:13 by nriker            #+#    #+#             */
-/*   Updated: 2021/03/01 23:14:20 by nriker           ###   ########.fr       */
+/*   Updated: 2021/03/08 10:11:31 by nriker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,7 @@
 void	invalid_hash_option(char *arg)
 {
 	ft_fprintf(STDERR_FILENO, "42sh: hash: %s: invalid option\n", arg);
-	ft_fprintf(STDERR_FILENO, "hash: usage: hash [-r] [name ... ]\n");
-}
-
-int			check_valid_flag_hash(char *arg, void (*print)(char *arg))
-{
-	int		i;
-
-	i = 0;
-	if (arg == NULL)
-		return (EXIT_SUCCESS);
-	if (arg[i] == '-')
-	{
-		print(arg);
-		return (EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
-}
-
-int			check_flag_r(char ***args, void (*invalid_print)(char *arg))
-{
-	if (**args != NULL && !ft_strcmp(**args, "-r"))
-	{
-		static_hash_action(del);
-		static_hash_action(init);
-		(*args)++;
-		while (**args != NULL && !ft_strcmp(**args, "-r"))
-			(*args)++;
-		return (check_valid_flag_hash(**args, invalid_print));
-	}
-	return (check_valid_flag_hash(**args, invalid_print));
+	ft_fprintf(STDERR_FILENO, "hash: usage: hash [-rd] [name ... ]\n");
 }
 
 int		check_hash_params(char **args)
@@ -64,6 +35,8 @@ int		check_hash_params(char **args)
 			else
 				ft_fprintf(STDERR_FILENO, "42sh: hash: %s: not found\n", args[i]);
 		}
+		else
+			print_hash(args[i]);
 		free(str);
 		i++;
 	}
@@ -85,7 +58,7 @@ int		builtin_hash(char **args, t_env envs, int subshell)
 		print_all_hash();
 		return (EXIT_SUCCESS);
 	}
-	if (check_flag_r(&args, invalid_hash_option) == EXIT_FAILURE)
+	if (check_hash_flags(&args, invalid_hash_option) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	return (check_hash_params(args));
 }
