559120b75bb91e9d8cee17c26e61764db5f192a4
tsorion <tsorion_9@mail.ru>
2021-03-13 02:38:56 +0300

    [EXEC] fc

---
+++ src/exec/builtin_fc.h
@@ -0,0 +1,40 @@
+#ifndef ENVIRONMENT_H
+# define ENVIRONMENT_H
+
+# include "readline.h"
+
+# define FC_NO_ERROR 1
+# define FC_EDITOR_NAME_ERROR 2
+# define FC_EDITOR_NAME_MISSING 3
+# define FC_USAGE_ERROR 4
+# define FC_HISTORY_NOT_FOUND 5
+# define FC_OUTSIDE_HISTSIZE 6
+
+# define FC_FLAG_R 0x00000001
+# define FC_FLAG_E 0x00000002
+# define FC_FLAG_L 0x00000004
+# define FC_FLAG_N 0x00000008
+# define FC_FLAG_S 0x00000010
+# define FC_SET_ALL_FLAGS 0x0000001F
+
+# define FC_MAX_EDITOR_NAME_SIZE 32
+# define FC_OPERAND_FIRST_DEFAULT_VALUE -16
+# define FC_DEFAULT_EDITOR "ed"
+
+typedef struct		s_fc_options
+{
+	unsigned int	flags;
+	int				first;
+	int				last;
+	char			editor[FC_MAX_EDITOR_NAME_SIZE];
+	t_history		*history_first;
+	int				number_of_history;
+}					t_fc_options;
+
+char				**parse_fc_flags_and_editor(t_fc_options *options,
+	char **args, int *error_code);
+void				parse_fc_operands(t_fc_options *options, char **args,
+	int *error_code);
+int					fc_flags_valid(const t_fc_options *options);
+
+#endif
---
+++ src/exec/parse_fc.c
@@ -0,0 +1,103 @@
+#include "libft.h"
+#include "builtin_fc.h"
+#include "readline.h"
+
+static void	search_str_in_history(const char *str, int *operand,
+	int *error_code)
+{
+	t_history	*history;
+	int			found;
+	int			history_number;
+
+	history_number = 0;
+	history = rp(NULL)->history;
+	found = 0;
+	while (history && !found && history_number < HISTSIZE)
+	{
+		if (ft_strfirststr(history->str, str))
+			found = 1;
+		history = history->next;
+		history_number++;
+	}
+	if (!found)
+		*error_code = FC_HISTORY_NOT_FOUND;
+	else if (history_number >= HISTSIZE)
+		*error_code = FC_OUTSIDE_HISTSIZE;
+	else
+		*operand = history_number * (-1);
+}
+
+static void	write_history_number_in_operand(int *operand, char *arg,
+	int *error_code)
+{
+	if (arg)
+	{
+		if (ft_isnumber(arg))
+		{
+			*operand = ft_atoi(arg);
+			if (*operand >= HISTSIZE || *operand <= HISTSIZE * (-1))
+				*error_code = FC_OUTSIDE_HISTSIZE;
+		}
+		else if (arg[0] != '-')
+			search_str_in_history(arg, operand, error_code);
+		else
+			*error_code = FC_USAGE_ERROR;
+	}
+}
+
+void		parse_fc_operands(t_fc_options *options, char **args, int *error_code)
+{
+	if (!(*args))
+	{
+		if (options->flags & FC_FLAG_E)
+			*error_code = FC_USAGE_ERROR;
+	}
+	else
+	{
+		write_history_number_in_operand(&(options->first), *args, error_code);
+		if (*error_code == FC_NO_ERROR)
+		{
+			args++;
+			write_history_number_in_operand(&(options->last), *args, error_code);
+		}
+	}
+}
+
+static void	parse_editor(t_fc_options *options, char *editor_name,
+	int *error_code)
+{
+	if (editor_name)
+		*error_code = FC_EDITOR_NAME_MISSING;
+	else if (ft_strlen(editor_name) >= FC_MAX_EDITOR_NAME_SIZE)
+		*error_code = FC_EDITOR_NAME_ERROR;
+	else
+		ft_strcpy(options->editor, editor_name);
+}
+
+char		**parse_fc_flags_and_editor(t_fc_options *options, char **args,
+	int *error_code)
+{
+	options->flags = 0;
+	while (*args && *error_code == FC_NO_ERROR)
+	{
+		if (!ft_strcmp(*args, "-e"))
+		{
+			options->flags |= FC_FLAG_E;
+			args++;
+			parse_editor(options, *args, error_code);
+		}
+		else if (!ft_strcmp(*args, "-r"))
+			options->flags |= FC_FLAG_R;
+		else if (!ft_strcmp(*args, "-l"))
+			options->flags |= FC_FLAG_L;
+		else if (!ft_strcmp(*args, "-s"))
+			options->flags |= FC_FLAG_S;
+		else if (!ft_strcmp(*args, "-n"))
+			options->flags |= FC_FLAG_N;
+		else
+			break;
+		if (*error_code == FC_NO_ERROR)
+			args++;
+	}
+	return (args);
+}
