ee9ed01505e4985efa76be59090ed94bc7c0472e
tsorion <tsorion_9@mail.ru>
2021-03-13 14:51:04 +0300

    [READLINE] search_histy_subtitution complete

--- src/readline/search_history_subtitution.c
+++ src/readline/search_history_subtitution.c
@@ -1,6 +1,94 @@
 #include "libft.h"
+#include "readline.h"
 
-char                *search_history_subtitution(char *n)
+static int	convert_history_number(int history_number, int number_of_history)
 {
-	return (ft_strdup("ZZZ"));
+	if (history_number > number_of_history)
+		history_number = number_of_history;
+	if (history_number < 0)
+	{
+		history_number = number_of_history + history_number + 1;
+		if (history_number < 1)
+			history_number = 1;
+	}
+	return (history_number);
+}
+
+static int init_number_of_history(int *number_of_history)
+{
+	t_history	*history;
+
+	history = rp(NULL)->history;
+	while (history && history->prev)
+		history = history->prev;
+	*number_of_history = 0;
+	while (history && history->next)
+	{
+		history = history->next;
+		*(number_of_history)++;
+	}
+}
+
+static char	*search_str_in_history(const char *str)
+{
+	t_history	*history;
+	int			found;
+	int			history_number;
+
+	history_number = 0;
+	history = rp(NULL)->history;
+	found = 0;
+	while (history && !found && history_number < HISTSIZE)
+	{
+		if (ft_strfirststr(history->str, str))
+			found = 1;
+		history = history->next;
+		history_number++;
+	}
+	if (!found || history_number >= HISTSIZE)
+		return (NULL);
+	else
+		return (ft_strdup(history->str));
+}
+
+static char	*search_history_str(int n)
+{
+	t_history	*history;
+	int			i;
+
+	i = 0;
+	history = rp(NULL)->history;
+	while (history && history->prev)
+		history = history->prev;
+	while (history && history->next && i < n)
+	{
+		history = history->next;
+		i++;
+	}
+	return (ft_strdup(history->str));
+}
+
+char		*search_history_subtitution(char *str)
+{
+	t_history	*history;
+	int			history_number;
+	int			number_of_history;
+
+	minus = 0;
+	history = rp(NULL)->history;
+	init_number_of_history(&number_of_history);
+	if (!str)
+		return (NULL);
+	if (ft_strcmp(str, "!!"))
+		history_number = -1;
+	if (ft_isnumber(str + 1))
+	{
+		history_number = ft_atoi(str + 1);
+		if (history_number >= HISTSIZE || history_number <= HISTSIZE * (-1))
+			return (NULL)
+	}
+	else
+		return (search_str_in_history(arg));
+	history_number = convert_history_number(history_number, number_of_history);
+	return (search_history_str(history_number));
 }
